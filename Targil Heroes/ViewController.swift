dc4deb638e7c57715805943e8c0166ec4a48ba96
emoonadev <35075841+emoonadev@users.noreply.github.com>
2018-06-26 10:03:26 +0300

    Initial Commit

---
+++ Targil Heroes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Targil Heroes
+//
+//  Created by Mickael Belhassen on 26/06/2018.
+//  Copyright © 2018 EmoonaDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
